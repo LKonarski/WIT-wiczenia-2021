b6b1114e1ba067bd36e56b28b3f8fbbc477a0c3d
LKonarski <91975018+LKonarski@users.noreply.github.com>
2021-10-19 00:23:59 +0200

    Add files via upload

---
+++ instrukcje_warunkowe1.py
@@ -0,0 +1,36 @@
+'''age = int(input('Podaj swój wiek: '))
+if age >= 18:
+    print('Jesteś pełnoletni/-a')
+else:
+    print('Nie masz ukończonych 18 lat')'''
+
+
+'''age = int(input('Podaj swój wiek: '))
+
+if age > 18:
+    print('Możesz prowadzić samochód oraz głosować w wyborach')
+elif age >= 17 and age <= 18:
+    print('Możesz prowadzić samochód')
+else:
+    print('Nie możesz głosować ani prowadzić samochodu')'''
+
+
+'''speed = int(input('Podaj przypusczczaną średnią prędkość: '))
+if speed >25:
+    print('Dojedziesz na miejsce szybciej niż pociąg')
+else:
+    print('Dojedziesz na miejsce wolniej niż pociąg')'''
+
+
+
+'''poczatkowy_kapital = int(input('Wprowadź swój początkowy kapitał: '))
+okres_inwestowania = int(input('Wprowadź swój okres inwestowania[miesiące]: '))
+koncowa_wartosc = int(input('Jaką kwotę chciałbyś uzyskać na koniec: '))
+
+lokata = round(poczatkowy_kapital*(1+0.02)**(okres_inwestowania))
+
+if koncowa_wartosc > lokata:
+    print('Lokata wychodzi mniejsza')
+else:
+    print('Lokata wychodzi większa')'''
+    
---
+++ petle1.py
@@ -0,0 +1,23 @@
+'''username = 'Admin'
+password = '1234'
+
+is_authenticated = False
+while not is_authenticated:
+    _username = input('Podaj login: ')
+    _password = input('Podaj hasło: ')
+    if username == _username and password == _password:
+        is_authenticated = True'''
+
+'''a = 24
+b = 36
+x = 0
+
+while True:
+    x += 1
+    if x % a == 0 and x % b == 0:
+        print(x)
+        break'''
+
+
+
+    
